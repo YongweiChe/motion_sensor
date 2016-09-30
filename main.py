2e67b2a9676dbbf1914685e652825a2be94e768d
Chundao Che <cche@umich.edu>
2016-09-30 10:41:34 -0400

    add email function

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
+import os
 import sensor
+import sys
+import time
 
 pin = [17, 4, 11, 18]
+email_enabled = True
+email = "hjhuwei1@gmail.com"
+
 while True:
 	if sensor.detect_motion(pin) is True:
-		print('motion detected')
+		if email_enabled is True:
+			sensor.send_email(email, "test", "test")
+		time.sleep(3600)
 
-		
+
--- sensor.py
+++ sensor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import time
+import subprocess
 from gpiozero import MotionSensor
 
 def detect_motion(pin):
@@ -12,4 +12,8 @@ def detect_motion(pin):
 		if(n.motion_detected is True):
 			return True
 	return False
+
+def send_email(email, subject, message):
+        send = 'echo ' +  message + ' | mutt -s ' + subject + ' -- ' + email
+        subprocess.call(send, shell=True)
 
