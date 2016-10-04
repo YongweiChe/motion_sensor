008d23767ad0a3cf0386a266752d0e5e77d32148
Yongwei Che <hjhuwei1@gmail.com>
2016-10-04 09:33:58 -0400

    added config parser

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python2
-import os
-import sensor
-import sys
 import time
+import sensor
+import datetime
+import ConfigParser
 
-pin = [17, 4, 11, 18]
-email_enabled = True
-email = "hjhuwei1@gmail.com"
+#reads settings.conf for pin, email_enabled, and email
+config = ConfigParser.RawConfigParser()
+conf = r'settings.conf'
+config.read(conf)
 
+pins_string = config.get('settings', 'pin').split(', ')
+pins = []
+for number in pins_string:
+	number =int(number)
+	pins.append(number)
+
+email_enabled = config.get('settings', 'email_enabled')
+email_enabled = email_enabled.strip(' ')
+email_enabled = email_enabled.lower()
+email = config.get('settings', 'email')
+
+subject = "ALERT MOTION DETECTED"
+message = "Motion detected on " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+#detects motion, if motion is detected, sleep for one hour
 while True:
-	if sensor.detect_motion(pin) is True:
-		if email_enabled is True:
-			sensor.send_email(email, "test", "test")
+	if sensor.detect_motion(pins) is True:
+		if email_enabled == 'true':
+			sensor.send_email(email, subject, message)
 		time.sleep(3600)
-
-
